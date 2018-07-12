d9d813b9e17496fa97151baac8edf88036744527
Gia Douglass <gigigia777@gmail.com>
2018-07-12 14:31:35 +0000

    Done.

--- least_coins.rb
+++ least_coins.rb
@@ -2,6 +2,23 @@
 
 def least_coins(cents)
 
-#Code your answer here!
-
+hash= {:quarters => 0, :dimes => 0, :nickels =>0, :pennies =>0}
+until cents<25
+    hash[:quarters] +=1
+    cents-=25
 end
+until cents<10
+    hash[:dimes] +=1
+    cents-=10
+end
+until cents<5
+    hash[:nickels] +=1
+    cents-=5
+end
+until cents<1
+    hash[:pennies] +=1
+    cents-=1
+end
+puts hash
+end
+least_coins(29)
